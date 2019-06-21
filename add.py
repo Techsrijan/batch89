8cdc01853c00eaf009b20024b9e25ddcc29b7bda
Ashwani <aswanibtech@gmail.com>
2019-06-21 09:26:06 +0530

    user input added

---
+++ add.py
@@ -0,0 +1,14 @@
+'''a=int(input("enter Any number"))
+b=int (input("enter Any number"))
+c = a + b
+print("sum=",c)'''
+
+name = input("enter your name")
+print(name)
+data= input("Are you a student if yes press y else n")
+print(data[0])
+print(data)
+
+data1= input("Are you a student if yes press y else n")[0]
+print(data1)
+print(data1)
