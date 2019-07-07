5eeb8b4cb82e41d8657231728f4eaa789de892d7
DELL <vishalg2597@gmail.com>
2019-07-07 18:51:22 +0530

    Number Guessing Game and Function Practice

---
+++ V_FunctionPractice.py
@@ -0,0 +1,140 @@
+#1 Factorial Program
+"""def fact(num):
+    i = 1
+    f = 1
+    while i <= num:
+        f = f * i
+        i = i + 1
+    return f
+
+
+n = int(input("Enter any number : "))
+a = fact(n)
+print("Factorial of", n, "is :", a)"""
+
+
+
+
+
+#2 Prime No.
+"""def prime(num):
+    i = 1
+    k = 0
+    while i <= num:
+        if num % i == 0:
+            k = k + 1
+        i = i + 1
+    return k
+
+n = int(input("Enter any number : "))
+a = prime(n)
+
+
+if a == 2:
+    print(n,": is a Prime Number ")
+else:
+    print(n,": is not a Prime Number ")"""
+
+
+
+
+
+
+#3 sum of all numbers in list
+"""
+def addlist(lst):
+    a = sum(lst)
+    return a
+
+l=[5,4,6,3,8]
+s= addlist(l)
+print("Sum of all no. in list =",s)"""
+
+
+
+
+
+
+
+# multiply of all numbers in list
+"""def prodlist(l):
+    prod = 1
+    for i in l:
+        prod *=i
+    return prod
+a=[5,4,6,3,8]
+p= prodlist(a)
+print("Sum of all no. in list =",p)"""
+
+
+
+
+# reverse a string
+"""def revstring(string):
+    rev = string[-1::-1]
+    return rev
+
+name = input("Enter the string : ")
+print("Reverse of",name,"is :",revstring(name))"""
+
+
+
+
+
+
+
+#8 print unique value from list
+
+"""def uniquelist(l):
+    return list(set(l))
+
+lst = [1,22,3,22,33,4,3,4,1,33,22,6,8,6]
+print("Unique List is :",uniquelist(lst))"""
+
+
+
+
+
+#10 print only even values in the list
+
+"""def evenlist(l):
+    x = []
+    for i in l:
+        if i % 2 == 0:
+            x.append(i)
+    return x
+
+lst = [1,22,3,22,33,4,3,4,1,33,22,6,8,6]
+print("Only Even no. is :",evenlist(lst))"""
+
+
+#12 check pass string is palindrome or not
+
+"""def checkPalindrome(string):
+    x = string[-1::-1]
+    if string == x:
+        return "String is Palindrome !!"
+    else:
+        return "String is not Palindrome !!"
+
+c = input("Enter the String : ")
+checkPalindrome(c)
+print(checkPalindrome(c))"""
+
+
+
+
+
+
+
+#print Square of every values in list
+def squarelist(l):
+    x = []
+    for i in l:
+        x.append(i**2)
+    return x
+
+#lst = [1,2,3,4,5,6,7,8,9,10]
+lst = list(range(1,21))
+print(lst)
+print("Square of every values in list :",squarelist(lst))
